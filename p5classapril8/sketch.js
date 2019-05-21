9b4350100b4ca9e4921e0108accae24239e4a02d
kyrach-lim <37121529+kyrach-lim@users.noreply.github.com>
2019-05-20 19:37:00 -0700

    p5edit

---
+++ p5classapril8/sketch.js
@@ -0,0 +1,30 @@
+function setup () {
+    createCanvas (500, 500);
+  button = createButton("more scary babies")
+  buttonSound.setVolume(.3);
+
+  buttons = selectAll = ('button');
+  console.log (buttons)
+
+  firstButton = select('#firstButton')
+  button.mousePressed();
+  //console.log(firstButton)
+
+  for 
+
+
+
+}
+
+function draw() {
+  background (140, 50, 30, 200);
+}
+
+function changeBackground() {
+  console.log ('button press is working')
+  background (0);
+}
+
+function mousePressed() {
+
+}
