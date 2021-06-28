15e515a1105ae66ddb99ac85731d2e703e1f2752
kyrach-lim <kyrachhiulim@gmail.com>
2021-06-28 10:24:09 -0700

    surveillance

---
+++ FinalProj/sketch.js
@@ -0,0 +1,42 @@
+'use strict'
+var video;
+let whitenoise;
+let combined;
+
+function preload() {
+  whitenoise = loadSound('media/whitenoise.mp3');
+  combined = loadSound('media/combined.mp3');
+}
+
+function setup() {
+  canvas = createCanvas(600, 350, WEBGL);
+  background(51);
+  canvas.id('p5canvas');
+  video = createCapture(VIDEO);
+  video.hide();
+  video.size(600, 350);
+  video.id('p5video');
+
+  whitenoise.loop();
+  combined.loop();
+
+  let seriously = new Seriously();
+  var src = seriously.source('#p5video');
+  var target = seriously.target('#p5canvas');
+
+  var nightvision = seriously.effect('nightvision');
+  nightvision.source = src;
+  target.source = nightvision;
+
+
+  seriously.go();
+}
+
+function mousePressed() {
+  if (whitenoise.isPlaying()) {
+    // .isPlaying() returns a boolean
+    whitenoise.stop(); // .play() will resume from .pause() position
+  } else {
+    whitenoise.play();
+  }
+}
